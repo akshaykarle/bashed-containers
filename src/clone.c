b81bf06a0f5c2e440e8097bc2cdc4be1e33309cc
Akshay Karle <akshay.a.karle@gmail.com>
2016-04-28 14:10:34 -0300

    add basic namespace cloning
    Resources:
    https://lwn.net/Articles/531114/
    http://man7.org/linux/man-pages/man7/namespaces.7.html
    http://man7.org/linux/man-pages/man2/clone.2.html
    https://www.gnu.org/software/libc/manual/html_node/Process-Completion.html

---
+++ src/clone.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+#define STACKSIZE 1024*1024
+static char child_stack[STACKSIZE];
+
+struct clone_args {
+  char **argv;
+};
+
+static int child_fn(void *stuff)
+{
+  struct clone_args *args = (struct clone_args *)stuff;
+
+  if (execvp(args->argv[0], args->argv) != 0) {
+    fprintf(stderr, "Command failed");
+    exit(1);
+  }
+
+  return -1;
+}
+
+int main(int argc, char *argv[])
+{
+  int flags = SIGCHLD;
+  struct clone_args args;
+  args.argv = &argv[1];
+
+  int child_pid = clone(child_fn, child_stack + STACKSIZE, flags, &args);
+  if (child_pid < 0) {
+    fprintf(stderr, "OMG clone failed. The world is coming to end!!\n");
+    return 1;
+  }
+
+  printf("Running child with PID: %d\n", child_pid);
+  if (waitpid(child_pid, NULL, 0) == -1) {
+    fprintf(stderr, "couldn't wait for child with PID: %d\n", child_pid);
+    return 1;
+  }
+  return 0;
+}
